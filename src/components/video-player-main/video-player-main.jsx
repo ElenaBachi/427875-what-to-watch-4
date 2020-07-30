d0c2f49e380d452aa312dd78e96cfcc0d62e681f
ElenaBachi <bachi.elena@gmail.com>
2020-07-30 15:15:47 +0300

    Исправляет ошибку в основном видеоплеере, устанавливает axios и redux-thunk

--- src/components/video-player-main/video-player-main.jsx
+++ src/components/video-player-main/video-player-main.jsx
@@ -1,29 +1,19 @@
 import React, {Fragment} from "react";
-import PropTypes from "prop-types";
 
 import withVideoPlayerMain from "../../hocs/with-video-player-main/with-video-player-main.jsx";
 
 import {connect} from "react-redux";
 
-const VideoPlayerMain = (props) => {
-  const {children} = props;
-
+const VideoPlayerMain = () => {
   return (
     <Fragment>
       <Fragment>
         <div className="player">
-          {children}
+
         </div>
       </Fragment>
     </Fragment>
   );
-};
-
-VideoPlayerMain.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node,
-  ]),
 };
 
 const mapStateToProps = (state) => ({
